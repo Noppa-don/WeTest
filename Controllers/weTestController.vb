a8b8b3bd8c476ecf99f6961a11aafff635bdfed2
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-07-03 09:38:03 +0700

    Create weTestController.vb

---
+++ Controllers/weTestController.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class weTestController
+        Inherits Controller
+        Function weTest() As ActionResult
+            Return View()
+        End Function
+        Function Registration() As ActionResult
+            Return View()
+        End Function
+
+        Function Activity() As ActionResult
+            Return View()
+        End Function
+
+        Function User() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
