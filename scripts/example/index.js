d4f90a40312dbee82585384eee9b8a52031114a7
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-07-03 09:38:11 +0700

    Create index.js

---
+++ scripts/example/index.js
@@ -0,0 +1,46 @@
+﻿var xSessionExpire = 'Session timeout';
+var xNoPermission = 'ไม่มีสิทธิเข้าใช้งาน';
+var xSave = false
+// ========================= Page Load ======================== //
+$(function () {
+	defaultLoad();
+});
+// ============================================================ //
+// ======================= Object Event ======================= //
+$(document).on('click', '#dList .dhead .line .sort', function (e, data) {
+}).on('click', '#importExcel', function (e, data) {
+	importExcel();
+});
+// ============================================================ //
+// =========================== Popup ========================== //
+$(document).on('click', '.popup .btnCenter,.popup .btnLeft', function (e, data) {
+	
+});
+// ============================================================ //
+// ========================= Function ========================= //
+function defaultLoad() {
+	$(window).on("throttledresize", throttledresizeHandler);
+	throttledresizeHandler();
+}
+function throttledresizeHandler() {
+}
+function hidexDetail(){
+}
+function showxDetail(){
+}
+function importExcel() {
+	var formData = new FormData();
+	formData.append('xFile', document.getElementById('fileExcel').files[0]);
+	$.ajax({
+		type: 'POST',
+		url: '/example/importExcel',
+		data: formData,
+		contentType: false,
+		processData: false,
+		success: function (data) {
+			alert(data);
+		}, complete: function () {
+		}
+	});
+}
+// ============================================================ //
