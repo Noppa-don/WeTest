98a41c6d3513b2712a2448c3abb5ecac51fbeadd
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-07-04 08:14:52 +0700

    Update weTestController.vb

--- Controllers/weTestController.vb
+++ Controllers/weTestController.vb
@@ -81,6 +81,152 @@ Namespace Controllers
             Return Json(L1, JsonRequestBehavior.AllowGet)
 
         End Function
+        <AcceptVerbs(HttpVerbs.Post)>
+        Function SaveUser()
+            Dim L1 As New List(Of clsMain)
+            Dim cn As SqlConnection, cmdMsSql As SqlCommand, dt As DataTable
+            Dim StdId As String = Guid.NewGuid.ToString
+            Dim objList As New clsMain()
+            Try
+                ' ================ Check Permission ================
+                cn = New SqlConnection(sqlCon("Wetest"))
+                If cn.State = 0 Then cn.Open()
+                ' ==================================================
+                cmdMsSql = cmdSQL(cn, "Insert into tblStudent(StudentID,FirstName,Surname,MobileNo,Email,Username,Password) 
+                                        values(@StudentID,@FirstName,@Surname,@MobileNo,@Email,@Username,@Password)")
+                With cmdMsSql
+                    .Parameters.Add("@StudentID", SqlDbType.VarChar).Value = StdId
+                    .Parameters.Add("@FirstName", SqlDbType.VarChar).Value = Request.Form("FirstName")
+                    .Parameters.Add("@Surname", SqlDbType.VarChar).Value = Request.Form("Surname")
+                    .Parameters.Add("@MobileNo", SqlDbType.VarChar).Value = Request.Form("MobileNo")
+                    .Parameters.Add("@Email", SqlDbType.VarChar).Value = Request.Form("Email")
+                    .Parameters.Add("@Username", SqlDbType.VarChar).Value = Request.Form("Username")
+                    .Parameters.Add("@Password", SqlDbType.VarChar).Value = oneWayKN(Request.Form("Password"))
+                    .ExecuteNonQuery()
+                End With
+
+                objList.dataType = "success"
+                objList.errorMsg = ""
+                L1.Add(objList)
+                objList = Nothing
+
+            Catch ex As Exception
+                objList.dataType = "error"
+                objList.errorMsg = ex.Message
+                L1.Add(objList)
+                objList = Nothing
+            Finally
+                If dt IsNot Nothing Then dt.Dispose() : dt = Nothing
+                If cmdMsSql IsNot Nothing Then cmdMsSql.Dispose() : cmdMsSql = Nothing
+                If cn IsNot Nothing Then If cn.State = 1 Then cn.Close() : cn.Dispose() : cn = Nothing
+            End Try
+            Return Json(L1, JsonRequestBehavior.AllowGet)
+        End Function
+        <AcceptVerbs(HttpVerbs.Post)>
+        Function CheckKeyCode()
+            Dim L1 As New List(Of clsMain)
+            Dim cn As SqlConnection, cmdMsSql As SqlCommand, dt As DataTable
+            Dim objList As New clsMain()
+            Try
+                ' ================ Check Permission ================
+                cn = New SqlConnection(sqlCon("Wetest"))
+                If cn.State = 0 Then cn.Open()
+                ' ==================================================
+                cmdMsSql = cmdSQL(cn, "select Keycode from tblKeycode where KeyCode =@KeyCode and IsActive = 1")
+                With cmdMsSql
+                    .Parameters.Add("@KeyCode", SqlDbType.VarChar).Value = Request.Form("KeyCode")
+                    .ExecuteNonQuery()
+                End With
+                dt = getDataTable(cmdMsSql)
+                If dt.Rows.Count = 0 Then
+                    objList.dataType = "error"
+                    objList.errorMsg = "WeTest Key is wrong !<br><br>Please try again or contact @Italt."
+                Else
+                    objList.dataType = "success"
+                    objList.errorMsg = "Registration Done !<br><br>Do you want go to Placement Test now ?. "
+
+                End If
+                L1.Add(objList)
+                objList = Nothing
+
+            Catch ex As Exception
+                objList.dataType = "error"
+                objList.errorMsg = ex.Message
+                L1.Add(objList)
+                objList = Nothing
+            Finally
+                If dt IsNot Nothing Then dt.Dispose() : dt = Nothing
+                If cmdMsSql IsNot Nothing Then cmdMsSql.Dispose() : cmdMsSql = Nothing
+                If cn IsNot Nothing Then If cn.State = 1 Then cn.Close() : cn.Dispose() : cn = Nothing
+            End Try
+            Return Json(L1, JsonRequestBehavior.AllowGet)
+        End Function
+        <AcceptVerbs(HttpVerbs.Post)>
+        Function CheckDiscount()
+            Dim L1 As New List(Of clsMain)
+            Dim cn As SqlConnection, cmdMsSql As SqlCommand, dt As DataTable
+            Dim objList As New clsMain()
+            Try
+                ' ================ Check Permission ================
+                cn = New SqlConnection(sqlCon("Wetest"))
+                If cn.State = 0 Then cn.Open()
+                ' ==================================================
+                cmdMsSql = cmdSQL(cn, "select DiscountId,DiscountType,DiscountAmount,case when expiredate >= getdate() then 0 else 1 end as isExpired,isUsed
+                                        from tblDiscount where IsActive = 1 and DiscountCode = @DiscountCode")
+                With cmdMsSql
+                    .Parameters.Add("@DiscountCode", SqlDbType.VarChar).Value = Request.Form("DiscountCode")
+                    .ExecuteNonQuery()
+                End With
+                dt = getDataTable(cmdMsSql)
+                If dt.Rows.Count = 0 Then
+                    objList.dataType = "error"
+                    objList.errorMsg = "This code does not apply to this promotion!<br>Please try again or Contact us @Italt<br><br>"
+                ElseIf dt.Rows(0)("isExpired").ToString = "1" Then
+                    objList.dataType = "Expired"
+                    objList.errorMsg = "This code is expired!<br>Please try again or Contact us @Italt<br><br>"
+                ElseIf dt.Rows(0)("isUsed").ToString = "True" Then
+                    objList.dataType = "Used"
+                    objList.errorMsg = "This code has alrady beeen used!<br>Please try again or contact @Italt<br><br>"
+                Else
+                    objList.dataType = "Success"
+                    Dim NetPrice As String = "0"
+                    Dim PackagePrice = ConfigurationManager.AppSettings("PackagePrice")
+
+                    If dt.Rows(0)("DiscountType").ToString = "1" Then
+                        NetPrice = (CInt(PackagePrice) - CInt(dt.Rows(0)("DiscountAmount"))).ToString
+                    Else
+                        NetPrice = (CInt(PackagePrice) - ((CInt(PackagePrice) * CInt(dt.Rows(0)("DiscountAmount"))) / 100)).ToString
+                    End If
+
+                    objList.errorMsg = NetPrice
+                End If
+                    L1.Add(objList)
+                objList = Nothing
+
+            Catch ex As Exception
+                objList.dataType = "error"
+                objList.errorMsg = ex.Message
+                L1.Add(objList)
+                objList = Nothing
+            Finally
+                If dt IsNot Nothing Then dt.Dispose() : dt = Nothing
+                If cmdMsSql IsNot Nothing Then cmdMsSql.Dispose() : cmdMsSql = Nothing
+                If cn IsNot Nothing Then If cn.State = 1 Then cn.Close() : cn.Dispose() : cn = Nothing
+            End Try
+            Return Json(L1, JsonRequestBehavior.AllowGet)
+        End Function
+        <AcceptVerbs(HttpVerbs.Post)>
+        Function GetPackagePrice()
+            Dim L1 As New List(Of clsMain)
+            Dim objList As New clsMain()
+            Dim PackagePrice = ConfigurationManager.AppSettings("PackagePrice")
+            objList.dataType = PackagePrice
+            L1.Add(objList)
+            objList = Nothing
+
+            Return Json(L1, JsonRequestBehavior.AllowGet)
+        End Function
+
 
 #End Region
 
@@ -94,6 +240,47 @@ Namespace Controllers
         Function User() As ActionResult
             Return View()
         End Function
+        <AcceptVerbs(HttpVerbs.Post)>
+        Function CheckUserLogin()
+            Dim L1 As New List(Of clsMain)
+            Dim cn As SqlConnection, cmdMsSql As SqlCommand, dt As DataTable
+            Dim objList As New clsMain()
+            Try
+                ' ================ Check Permission ================
+                cn = New SqlConnection(sqlCon("Wetest"))
+                If cn.State = 0 Then cn.Open()
+                ' ==================================================
+                cmdMsSql = cmdSQL(cn, "select StudentID from tblStudent where Username = @Username and Password = @Password and IsActive = 1")
+                With cmdMsSql
+                    .Parameters.Add("@Username", SqlDbType.VarChar).Value = Request.Form("Username")
+                    .Parameters.Add("@Password", SqlDbType.VarChar).Value = oneWayKN(Request.Form("Password"))
+                    .ExecuteNonQuery()
+                End With
+                dt = getDataTable(cmdMsSql)
+                If dt.Rows.Count = 0 Then
+                    objList.dataType = "error"
+                    objList.errorMsg = "Username or Password is wrong !<br><br>Please try again or contact @Italt."
+                Else
+                    objList.dataType = "success"
+                    objList.errorMsg = ""
+
+                End If
+                L1.Add(objList)
+                objList = Nothing
+
+            Catch ex As Exception
+                objList.dataType = "error"
+                objList.errorMsg = ex.Message
+                L1.Add(objList)
+                objList = Nothing
+            Finally
+                If dt IsNot Nothing Then dt.Dispose() : dt = Nothing
+                If cmdMsSql IsNot Nothing Then cmdMsSql.Dispose() : cmdMsSql = Nothing
+                If cn IsNot Nothing Then If cn.State = 1 Then cn.Close() : cn.Dispose() : cn = Nothing
+            End Try
+            Return Json(L1, JsonRequestBehavior.AllowGet)
+        End Function
+
 #End Region
 
 
