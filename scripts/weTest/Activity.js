8f951d9bc3c5bc203a04e047b2b2fe6ca363dc81
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-07-08 15:44:06 +0700

    Update Activity.js

--- scripts/weTest/Activity.js
+++ scripts/weTest/Activity.js
@@ -2,7 +2,7 @@
 // ========================= Page Load ======================== //
 $(function () { $('div[data-role=page]').page({ theme: 'c', }); $('#Firstname').focus(); });
 
-SaveNextAnswerAndGetQuestion()
+GetQuestionAndAnswer()
 
 // ============================================================ //
 // ======================= Object Event ======================= //
@@ -11,8 +11,11 @@ $(document)
 
 
  .on('click', '.btnNext', function (e, data) {
-     console.log('#btnNext');
-     SaveNextAnswerAndGetQuestion();
+     if ($('.btnNext').hasClass('UnActive') == false) {GetQuestionAndAnswer('next'); }
+
+ })
+ .on('click', '.btnBack', function (e, data) {
+     if ($('.btnBack').hasClass('UnActive') == false) { GetQuestionAndAnswer('back'); }
  })
 
 // ============================================================ //
@@ -64,6 +67,34 @@ function SaveNewUser() {
     });
 }
 
+function GetQuestionAndAnswer(ActionType) {
+    var post1 = 'ActionType=' + ActionType;
+    $.ajax({
+        type: 'POST',
+        url: '/weTest/GetQuestionAndAnswer',
+        data: post1,
+        success: function (data) {
+    
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].ItemType == 1) {
+                    
+                    $('#divQuestion').html(data[i].Itemtxt);
+                    $('#divQuestion').attr('Qid', data[i].ItemId);
+
+                    console.log(data[i].ItemStatus);
+
+                    if (data[i].ItemStatus == 'first') { $('.btnBack').addClass("UnActive"); }
+                    if (data[i].ItemStatus == 'last') { $('.btnNext').addClass("UnActive"); }
+
+                    if (data[i].ItemStatus == 'second') { $('.btnBack').removeClass("UnActive"); }
+                    if (data[i].ItemStatus == 'beforelast') { $('.btnNext').removeClass("UnActive"); }
+                } else {
+                    $('#divAnswer').html(data[i].Itemtxt);
+                }
+            }
+        }
+    });
+}
 function SaveNextAnswerAndGetQuestion() {
     $.ajax({
         type: 'POST',
