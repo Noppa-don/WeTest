a6fa5732e57dabba3d00b908585c550d5abfcf42
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-07-09 07:48:23 +0700

    Update Activity.js

--- scripts/weTest/Activity.js
+++ scripts/weTest/Activity.js
@@ -11,6 +11,14 @@ $(document)
 
  .on('click', '.btnNext', function (e, data) {
      if ($('.btnNext').hasClass('UnActive') == false) {GetQuestionAndAnswer('next'); }
+<<<<<<< HEAD
+=======
+
+ })
+ .on('click', '.btnBack', function (e, data) {
+     if ($('.btnBack').hasClass('UnActive') == false) { GetQuestionAndAnswer('back'); }
+ })
+>>>>>>> eff8a4b6f0e0a1b468cf44d1e0cb047bdc368839
 
  })
  .on('click', '.btnBack', function (e, data) {
@@ -58,10 +66,47 @@ function GetQuestionAndAnswer(ActionType) {
     
             for (var i = 0; i < data.length; i++) {
 
+<<<<<<< HEAD
                 if (data[i].ItemStatus == 'sessionExpired') {
                     window.location = '/Wetest/User';
                 }
 
+=======
+function GetQuestionAndAnswer(ActionType) {
+    var post1 = 'ActionType=' + ActionType;
+    $.ajax({
+        type: 'POST',
+        url: '/weTest/GetQuestionAndAnswer',
+        data: post1,
+        success: function (data) {
+    
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].ItemType == 1) {
+                    
+                    $('#divQuestion').html(data[i].Itemtxt);
+                    $('#divQuestion').attr('Qid', data[i].ItemId);
+
+                    console.log(data[i].ItemStatus);
+
+                    if (data[i].ItemStatus == 'first') { $('.btnBack').addClass("UnActive"); }
+                    if (data[i].ItemStatus == 'last') { $('.btnNext').addClass("UnActive"); }
+
+                    if (data[i].ItemStatus == 'second') { $('.btnBack').removeClass("UnActive"); }
+                    if (data[i].ItemStatus == 'beforelast') { $('.btnNext').removeClass("UnActive"); }
+                } else {
+                    $('#divAnswer').html(data[i].Itemtxt);
+                }
+            }
+        }
+    });
+}
+function SaveNextAnswerAndGetQuestion() {
+    $.ajax({
+        type: 'POST',
+        url: '/weTest/SaveNextAnswerAndGetQuestion',
+        success: function (data) {
+            for (var i = 0; i < data.length; i++) {
+>>>>>>> eff8a4b6f0e0a1b468cf44d1e0cb047bdc368839
                 if (data[i].ItemType == 1) {
                     $('#divQuestion').html(data[i].Itemtxt);
                     $('#divQuestion').attr('Qid', data[i].ItemId);
