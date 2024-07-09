664743952445f494ff5f391c5098c7e36d06163a
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-07-09 07:06:41 +0700

    Update Activity.js

--- scripts/weTest/Activity.js
+++ scripts/weTest/Activity.js
@@ -2,79 +2,78 @@
 // ========================= Page Load ======================== //
 $(function () { $('div[data-role=page]').page({ theme: 'c', }); $('#Firstname').focus(); });
 
-SaveNextAnswerAndGetQuestion()
+GetQuestionAndAnswer()
 
 // ============================================================ //
 // ======================= Object Event ======================= //
 $(document)
 // ======================= Activity ======================= //
 
-
  .on('click', '.btnNext', function (e, data) {
-     console.log('#btnNext');
-     SaveNextAnswerAndGetQuestion();
- })
+     if ($('.btnNext').hasClass('UnActive') == false) {GetQuestionAndAnswer('next'); }
 
+ })
+ .on('click', '.btnBack', function (e, data) {
+     if ($('.btnBack').hasClass('UnActive') == false) { GetQuestionAndAnswer('back'); }
+ })
+ .on('click', '.divAnswerbar', function (e, data) {
+     $('.divAnswerbar').removeClass("Answered");
+     $(this).addClass("Answered");
+
+     var post1 = 'AnsweredId=' + $(this).attr('ansid') + '&QuestionId=' + $(this).attr('QId');
+
+     $.ajax({
+         type: 'POST',
+         url: '/weTest/SaveAnswed',
+         data: post1,
+         success: function (data) {
+
+         }
+     });
+ })
+ .on('click', '#divSendQuiz', function (e, data) {
+     $.ajax({
+         type: 'POST',
+         url: '/weTest/EndQuiz',
+         success: function (data) {
+             for (var i = 0; i < data.length; i++) {
+                 if (data[i].dataType == 'success2') {
+                     window.location = '/Wetest/Activity';
+                 }
+             }
+         }
+     });
+ })
 // ============================================================ //
 
 // ========================= Function ========================= //
 
-function checkInvalidRegisterData() {
-
-    var CheckError = 'true';
-
-    if ($('#Firstname').val() == '') { $('#Firstname').addClass("InvalidData"); CheckError = 'false'; }
-    if ($('#Surname').val() == '') { $('#Surname').addClass("InvalidData"); CheckError = 'false'; }
-    if ($('#MobileNo').val() == '' || $('#MobileNo').val().length != 10) { $('#MobileNo').addClass("InvalidData"); CheckError = 'false'; }
-    if ($('#EMail').val() == '') { $('#EMail').addClass("InvalidData"); CheckError = 'false'; }
-    if ($('#Username').val() == '') { $('#Username').addClass("InvalidData"); CheckError = 'false'; }
-    if ($('#Password').val() == '') { $('#Password').addClass("InvalidData"); CheckError = 'false'; }
-    if ($('#ConfirmPassword').val() == '') { $('#ConfirmPassword').addClass("InvalidData"); CheckError = 'false'; }
-
-    if ($('#ConfirmPassword').val() != $('#Password').val()) {
-        $('#Password, #ConfirmPassword').addClass("InvalidData");
-        CheckError = 'false';
-    }
-
-    if ($('#btnStudent, #btnOther').hasClass("btnSelected")) { } else {
-        $('#btnStudent, #btnOther').addClass("InvalidData");
-        CheckError = 'false';
-    }
-
-    return CheckError
-}
-
-function SaveNewUser() {
-    var post1 = 'FirstName=' + $('.spFirstname').text() + '&Surname=' + $('.spSurname').text() + '&MobileNo=' + $('.spMobileNo').text() +
-        '&EMail=' + $('.spEMail').text() + '&Username=' + $('.spUsername').text() + '&Password=' + $('#Password').val() + '&StudentType=' + StudentType;
+function GetQuestionAndAnswer(ActionType) {
+    var post1 = 'ActionType=' + ActionType;
     $.ajax({
         type: 'POST',
-        url: '/weTest/SaveUser',
+        url: '/weTest/GetQuestionAndAnswer',
         data: post1,
         success: function (data) {
+    
             for (var i = 0; i < data.length; i++) {
-                switch (data[i].dataType) {
-                    case 'error':
-                        console.log(data[i].errorMsg);
-                    case 'success':
-                        UploadStudentPhoto();
-                }
-            }
-        }
-    });
-}
 
-function SaveNextAnswerAndGetQuestion() {
-    $.ajax({
-        type: 'POST',
-        url: '/weTest/SaveNextAnswerAndGetQuestion',
-        success: function (data) {
-            for (var i = 0; i < data.length; i++) {
+                if (data[i].ItemStatus == 'sessionExpired') {
+                    window.location = '/Wetest/User';
+                }
+
                 if (data[i].ItemType == 1) {
-                    $('#divQuestion').html(data[i].ItemNo + '. ' + data[i].Itemtxt);
+                    $('#divQuestion').html(data[i].Itemtxt);
                     $('#divQuestion').attr('Qid', data[i].ItemId);
+
+                    console.log(data[i].ItemStatus);
+
+                    if (data[i].ItemStatus == 'first') { $('.btnBack').addClass("UnActive"); }
+                    if (data[i].ItemStatus == 'last') { $('.btnNext').addClass("UnActive"); }
+
+                    if (data[i].ItemStatus == 'second') { $('.btnBack').removeClass("UnActive"); }
+                    if (data[i].ItemStatus == 'beforelast') { $('.btnNext').removeClass("UnActive"); }
                 } else {
-                    //console.log('A : no ' + data[i].ItemNo + ' id ' + data[i].ItemId + ' txt ' + data[i].Itemtxt);
                     $('#divAnswer').html(data[i].Itemtxt);
                 }
             }
