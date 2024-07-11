d8091f8807d5620fbed042891e626d450eb4d01e
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-07-11 07:18:52 +0700

    Create Practice.js

---
+++ scripts/weTest/Practice.js
@@ -0,0 +1,11 @@
+﻿var OTPNum
+// ========================= Page Load ======================== //
+$(function () { $('div[data-role=page]').page({ theme: 'c', }); });
+// ============================================================ //
+// ======================= Object Event ======================= //
+$(document)
+// ======================= Activity ======================= //
+ .on('click', '#btnLesson', function (e, data) {
+     $('#LessonType').removeClass("ui-hide");
+ })
+ 
