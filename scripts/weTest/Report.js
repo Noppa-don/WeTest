8a617ddba355a10b279603887199f7121339baf3
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-07-19 08:09:08 +0700

    Update Report.js

--- scripts/weTest/Report.js
+++ scripts/weTest/Report.js
@@ -1,4 +1,4 @@
-﻿var ExamAmount, skill
+﻿var DateType, selectedDate, formatSelectedDate;
 // ========================= Page Load ======================== //
 $(function () { $('div[data-role=page]').page({ theme: 'c', }); });
 // ============================================================ //
@@ -15,16 +15,64 @@ $(document)
  })
  .on('click', '.btnBack', function (e, data) {
      window.location = '/Wetest/User';
- })
- 
-//20240717 Choose Random All Skill
+ }) 
+//20240718 Choose Random All Skill
  .on('click', '#btnRandomAll', function (e, data) {
      $('#btnRandomAll').toggleClass('btnSelected');
      $('.btnSkill').toggleClass('Selected');
  })
-
-//20240717 Choose Random Skill
+//20240718 Choose Random Skill
  .on('click', '.btnSkill', function (e, data) {
      $(this).toggleClass('Selected');
      $('#btnRandomAll').removeClass('Selected');
  })
+//20240718 open dialog start date
+ .on('click', '#btnStartDate', function (e, data) {
+     DateType = 'start';
+     OpenFilterdate();
+ })
+//20240718 open dialog end date
+ .on('click', '#btnEndDate', function (e, data) {
+     DateType = 'end';
+     OpenFilterdate();
+ })
+//20240718 -- Close Dialog
+ .on('click', '.ui-icon.close', function (e, data) {
+        popupClose($(this).closest('.my-popup'));
+    })
+//20240718 -- Choose StrartDate
+ .on('click', '#btnSelectDate', function (e, data) {
+        if (DateType == 'start') {
+            $('#StartDate').val(formatSelectedDate);
+        } else {
+            $('#EndDate').val(formatSelectedDate);
+        }
+        popupClose($(this).closest('.my-popup'));
+    })
+
+//20240718 Dialog Select filter daate
+function OpenFilterdate() {
+
+    $('.btnSelectDate').addClass('unActive');
+
+    $('#spnGoalName').html('Select ' + DateType + ' date');
+
+    var options = $.extend(global.datepickerOption, {
+        maxDate: 1,
+        onSelect: function () {
+            var selected = $("#SelectFilterDate").datepicker("getDate");
+            selectedDate = selected.getFullYear() + '-' + (selected.getMonth() + 1) + '-' + selected.getDate();
+            formatSelectedDate = selected.getDate() + '/' + (selected.getMonth() + 1) + '/' + selected.getFullYear();
+
+            $('#spnShowdate').html('Your ' + DateType + ' Date : ' + formatSelectedDate)
+            $('#btnSelectDate').removeClass('unActive');
+           
+        }
+    });
+
+    $("#SelectFilterDate").datepicker(options);
+
+    $('#dialogFilterDate').attr('action', 'focus');
+    popupOpen($('#dialogFilterDate'), 99999);
+
+}
