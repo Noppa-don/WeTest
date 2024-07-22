2d8e264b72bbf6d82dad1947903c7c9cae7b6187
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-07-22 08:03:12 +0700

    Update Report.js

--- scripts/weTest/Report.js
+++ scripts/weTest/Report.js
@@ -1,4 +1,4 @@
-﻿var DateType, selectedDate, formatSelectedDate;
+﻿var IsLesson, selectedStartDate, selectedEndDate, formatSelectedDate;
 // ========================= Page Load ======================== //
 $(function () { $('div[data-role=page]').page({ theme: 'c', }); });
 // ============================================================ //
@@ -8,11 +8,14 @@ $(document)
  .on('click', '#btnLesson', function (e, data) {
      $('#btnLesson').addClass('btnSelected');
      $('#btnRandom').removeClass('btnSelected');
+     PracticeType = '1'
  })
  .on('click', '#btnRandom', function (e, data) {
      $('#btnLesson').removeClass('btnSelected');
      $('#btnRandom').addClass('btnSelected');
+     PracticeType = '0'
  })
+
  .on('click', '.btnBack', function (e, data) {
      window.location = '/Wetest/User';
  }) 
@@ -29,11 +32,21 @@ $(document)
 //20240718 open dialog start date
  .on('click', '#btnStartDate', function (e, data) {
      DateType = 'start';
+     if ($('#StartDate').val() != '') {
+         $('#spnShowdate').html('Your start date : ' + $('#StartDate').val());
+     } else {
+         $('#spnShowdate').addClass('ui-hide');
+     }
      OpenFilterdate();
  })
 //20240718 open dialog end date
  .on('click', '#btnEndDate', function (e, data) {
      DateType = 'end';
+     if ($('#EndDate').val() != '') {
+         $('#spnShowdate').html('Your end date : ' + $('#EndDate').val());
+     } else {
+         $('#spnShowdate').addClass('ui-hide');
+     }
      OpenFilterdate();
  })
 //20240718 -- Close Dialog
@@ -49,24 +62,66 @@ $(document)
         }
         popupClose($(this).closest('.my-popup'));
     })
+//20240718 -- Search
+ .on('click', '#btnSearch', function (e, data) {
+     var startdate, enddate;
+     if ($('#rdbThisWeek').is(":checked")) {
+         startdate = 'week';
+     }
+     if ($('#rdbThisMonth').is(":checked")) {
+         startdate = 'month';
+     }
+     if ($('#rdbChooseDate').is(":checked")) {
+         startdate = selectedStartDate
+         enddate = selectedEndDate
+     }
+
+     var post1 = 'StartDate=' + startdate + '&EndDate=' + enddate + '&PracticeType=' + PracticeType;
+
+     $.ajax({
+         type: 'POST',
+         url: '/weTest/GetReport',
+         data: post1,
+         success: function (data) {
+             for (var i = 0; i < data.length; i++) {
+                 if (data[i].Result == 'success') {
+                    
+                 } else {
+                     $('#dialogAlert').attr('action', 'focus');
+                     $('#dialogAlert .ui-text').html(data[i].Msg);
+                     popupOpen($('#dialogAlert'), 99999);
+                 }
+
+             }
+         }
+     });
+ })
+
 
 //20240718 Dialog Select filter daate
 function OpenFilterdate() {
 
     $('.btnSelectDate').addClass('unActive');
 
-    $('#spnGoalName').html('Select ' + DateType + ' date');
+    $('#spnDateName').html('Select ' + DateType + ' date');
 
     var options = $.extend(global.datepickerOption, {
         maxDate: 1,
         onSelect: function () {
             var selected = $("#SelectFilterDate").datepicker("getDate");
-            selectedDate = selected.getFullYear() + '-' + (selected.getMonth() + 1) + '-' + selected.getDate();
-            formatSelectedDate = selected.getDate() + '/' + (selected.getMonth() + 1) + '/' + selected.getFullYear();
 
-            $('#spnShowdate').html('Your ' + DateType + ' Date : ' + formatSelectedDate)
+            if (DateType == 'start') {
+                selectedStartDate = selected.getFullYear() + '-' + (selected.getMonth() + 1) + '-' + selected.getDate();
+            } else {
+                selectedEndDate = selected.getFullYear() + '-' + (selected.getMonth() + 1) + '-' + selected.getDate();
+            }
+
+            formatSelectedDate = selected.getDate() + '/' + (selected.getMonth() + 1) + '/' + selected.getFullYear()
+
+            $('#spnShowdate').html('Your ' + DateType + ' date : ' + formatSelectedDate);
+            $('#spnShowdate').removeClass('ui-hide');
             $('#btnSelectDate').removeClass('unActive');
-           
+
         }
     });
 
