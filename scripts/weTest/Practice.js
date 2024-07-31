a865a3a5c4fd673fc8aad5f26a05df3fc508bfde
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-07-31 08:21:08 +0700

    Update Practice.js

--- scripts/weTest/Practice.js
+++ scripts/weTest/Practice.js
@@ -2,6 +2,7 @@
 // ========================= Page Load ======================== //
 $(function () { $('div[data-role=page]').page({ theme: 'c', }); });
 CheckLoginStatus();
+GetLevel();
 // ============================================================ //
 // ======================= Object Event ======================= //
 $(document)
@@ -9,11 +10,137 @@ $(document)
  .on('click', '#btnLesson', function (e, data) {
      $('#btnLesson').addClass('btnSelected');
      $('#btnRandom').removeClass('btnSelected');
+
+
+     $('#LessonType').removeClass("ui-hide");
+     $('#RandomType,#skillRandom,.btnStart').addClass("ui-hide");
+ })
+ .on('click', '#btnRandom', function (e, data) {
+     $('#btnLesson').removeClass('btnSelected');
+     $('#btnRandom').addClass('btnSelected');
+
+     $('#LessonType').addClass("ui-hide");
+     $('#RandomType ,#skillRandom,.btnStart').removeClass("ui-hide");
+ })
+ .on('click', '#ReadingOther', function (e, data) {
+
+     $('#PracticeType,#LessonType').addClass('ui-hide');
+     $('#AllReading,.footer').removeClass('ui-hide');
+
+ })
+ .on('click', '#ListeningOther', function (e, data) {
+
+     $('#PracticeType,#LessonType').addClass('ui-hide');
+     $('#AllListening,.footer').removeClass('ui-hide');
+ })
+ .on('click', '#GrammarOther', function (e, data) {
+
+     $('#PracticeType,#LessonType').addClass('ui-hide');
+     $('#AllGrammar,.footer').removeClass('ui-hide');
+ })
+ .on('click', '#SituationOther', function (e, data) {
+
+     $('#PracticeType,#LessonType').addClass('ui-hide');
+     $('#AllSituation,.footer').removeClass('ui-hide');
+ })
+ .on('click', '#VocabularyOther', function (e, data) {
+
+     $('#PracticeType,#LessonType').addClass('ui-hide');
+     $('#AllVocabulary,.footer').removeClass('ui-hide');
+ })
+ .on('click', '.btnBack', function (e, data) {
+     window.location = '/Wetest/User';
+ })
+ .on('click', '.Lessondiv', function (e, data) {
+
+     var TestsetName = $(this).text();
+     var TestsetId = $(this).attr('id');
+     $('#dialogSelect').attr('action', 'focus');
+     $('#dialogSelect .ui-text').html('Do you want to start practice ' + TestsetName + ' now!');
+     $('#dialogSelect .btnSelected').attr('TestsetId', TestsetId);
+     $('#dialogSelect  .btnSelected').attr('TestsetName', TestsetName);
+     popupOpen($('#dialogSelect'), 99999);
+ })
+ .on('click', '.ui-icon.close ', function (e, data) {
+     popupClose($(this).closest('.my-popup'));
+ })
+ .on('click', '#dialogSelect .btnSelected', function (e, data) {
+     console.log($(this).attr('TestsetName'))
+     popupClose($(this).closest('.my-popup'));
+     GotoPractice($(this).attr('TestsetId'), $(this).attr('TestsetName'));
+ })
+ .on('click', '#dialogSelect .btnCancel', function (e, data) {
+
+     popupClose($(this).closest('.my-popup'));
+
+ })
+ //20240717 Choose Exam Amount
+ .on('click', '.btnAmount', function (e, data) {
+     $('.btnAmount').removeClass('btnSelected');
+     $('#UserType').val('');
+     $(this).addClass('btnSelected');
+ })
+ //20240717 Check numeric key press
+ .on('keypress', '#UserType', function (e, data) {
+     if (event.which != 8 && isNaN(String.fromCharCode(event.which))) {
+         event.preventDefault(); //stop character from entering input
+     }
+ })
+//20240717 Choose Random All Skill
+ .on('click', '#btnRandomAll', function (e, data) {
+     $('#btnRandomAll').toggleClass('btnSelected');
+     $('.btnSkill').toggleClass('Selected');
+ })
+//20240717 Choose Random Skill
+ .on('click', '.btnSkill', function (e, data) {
+     $(this).toggleClass('Selected');
+     $('#btnRandomAll').removeClass('Selected');
+ })
+//20240717 Start Random
+ .on('click', '.btnStart', function (e, data) {
+     if ($(".btnAmount.btnSelected").attr('id') == 'btnUserType') {
+         ExamAmount = $("#UserType").val();
+     } else {
+         ExamAmount = $(".btnAmount.btnSelected").text();
+     }
+     var arrSkill = new Array();
+
+     if ($("#btnRandomAll").hasClass('btnSelected') == true) {
+         arrSkill.push('All');
+     } else {
+         $('.Selected').each(function (i, obj) {
+             arrSkill.push($(this).attr('id'));
+         });
+     }
+
+     skill = arrSkill.join(',');
+
+     var post1 = 'ExamAmount=' + ExamAmount + '&arrSkill=' + skill;
+     $.ajax({
+         type: 'POST',
+         url: '/weTest/RandomPractice',
+         data: post1,
+         success: function (data) {
+             for (var i = 0; i < data.length; i++) {
+                 if (data[i].dataType == 'success') {
+                     console.log(data[i].errorMsg);
+                     GotoPractice(data[i].errorMsg, 'RandomPractice');
+                 }
+             }
+         }
+     });
+ })
+//20240730 Select Dropdown
+ .on('click', '#ddlLevel', function (e, data) {
+     console.log(this.value);
+     var optionSelected = $("option:selected", this);
+     console.log('----');
+     console.log(optionSelected);
+     var post1 = 'LevelId=' + this.value;
      $.ajax({
          type: 'POST',
          url: '/weTest/GetLesson',
-         contentType: false,
-         processData: false,
+         data: post1,
          success: function (data) {
              for (var i = 0; i < data.length; i++) {
                  if (data[i].skillSet == 'error') {
@@ -32,128 +159,10 @@ $(document)
              }
          }
      });
-
-     $('#LessonType').removeClass("ui-hide");
-     $('#RandomType,#skillRandom,.btnStart').addClass("ui-hide");
- })
- .on('click', '#btnRandom', function (e, data) {
-     $('#btnLesson').removeClass('btnSelected');
-     $('#btnRandom').addClass('btnSelected');
-
-     $('#LessonType').addClass("ui-hide");
-     $('#RandomType ,#skillRandom,.btnStart').removeClass("ui-hide");
- })
- .on('click', '#ReadingOther', function (e, data) {
-  
-     $('#PracticeType,#LessonType').addClass('ui-hide');
-     $('#AllReading,.footer').removeClass('ui-hide');
-
- })
- .on('click', '#ListeningOther', function (e, data) {
-
-     $('#PracticeType,#LessonType').addClass('ui-hide');
-     $('#AllListening,.footer').removeClass('ui-hide');
- })
- .on('click', '#GrammarOther', function (e, data) {
-    
-     $('#PracticeType,#LessonType').addClass('ui-hide');
-     $('#AllGrammar,.footer').removeClass('ui-hide');
- })
- .on('click', '#SituationOther', function (e, data) {
-  
-     $('#PracticeType,#LessonType').addClass('ui-hide');
-     $('#AllSituation,.footer').removeClass('ui-hide');
- })
- .on('click', '#VocabularyOther', function (e, data) {
-
-     $('#PracticeType,#LessonType').addClass('ui-hide');
-     $('#AllVocabulary,.footer').removeClass('ui-hide');
- })
- .on('click', '.btnBack', function (e, data) {
-     window.location = '/Wetest/User';
- })
- .on('click', '.Lessondiv', function (e, data) {
-
-     var TestsetName = $(this).text();
-        var TestsetId = $(this).attr('id');
-        $('#dialogSelect').attr('action', 'focus');
-        $('#dialogSelect .ui-text').html('Do you want to start practice ' + TestsetName + ' now!');
-        $('#dialogSelect .btnSelected').attr('TestsetId', TestsetId);
-        $('#dialogSelect  .btnSelected').attr('TestsetName', TestsetName);
-        popupOpen($('#dialogSelect'), 99999);
-    })
- .on('click', '.ui-icon.close ', function (e, data) {
-      popupClose($(this).closest('.my-popup'));
- })
- .on('click', '#dialogSelect .btnSelected', function (e, data) {
-     console.log($(this).attr('TestsetName'))
-      popupClose($(this).closest('.my-popup'));
-      GotoPractice($(this).attr('TestsetId'), $(this).attr('TestsetName'));
- })
- .on('click', '#dialogSelect .btnCancel', function (e, data) {
-
-     popupClose($(this).closest('.my-popup'));
-
- })
- //20240717 Choose Exam Amount
- .on('click', '.btnAmount', function (e, data) {
-     $('.btnAmount').removeClass('btnSelected');
-     $('#UserType').val('');
-     $(this).addClass('btnSelected');
- })
- //20240717 Check numeric key press
- .on('keypress', '#UserType', function (e, data) {
-    if (event.which != 8 && isNaN(String.fromCharCode(event.which))) {
-        event.preventDefault(); //stop character from entering input
-    }
- })
-//20240717 Choose Random All Skill
- .on('click', '#btnRandomAll', function (e, data) {
-     $('#btnRandomAll').toggleClass('btnSelected');
-     $('.btnSkill').toggleClass('Selected');
- })
-//20240717 Choose Random Skill
- .on('click', '.btnSkill', function (e, data) {
-     $(this).toggleClass('Selected');
-     $('#btnRandomAll').removeClass('Selected');
- })
-//20240717 Start Random
- .on('click', '.btnStart', function (e, data) {
-     if ($(".btnAmount.btnSelected").attr('id') == 'btnUserType') {
-         ExamAmount = $("#UserType").val();
-     } else {
-        ExamAmount = $(".btnAmount.btnSelected").text();
-     }
-     var arrSkill = new Array();
-
-     if ($("#btnRandomAll").hasClass('btnSelected') == true) {
-         arrSkill.push('All');
-     } else {
-         $('.Selected').each(function (i, obj) {
-             arrSkill.push($(this).attr('id'));
-         });
-     }
-
-     skill = arrSkill.join(',');
-
-        var post1 = 'ExamAmount=' + ExamAmount + '&arrSkill=' + skill;
-     $.ajax({
-         type: 'POST',
-         url: '/weTest/RandomPractice',
-         data: post1,
-         success: function (data) {
-             for (var i = 0; i < data.length; i++) {
-                 if (data[i].dataType == 'success') {
-                     console.log(data[i].errorMsg);
-                     GotoPractice(data[i].errorMsg,'RandomPractice');
-                 }
-             }
-         }
-     });
  })
 
 
-function GotoPractice(TestsetId,TestsetName) {
+function GotoPractice(TestsetId, TestsetName) {
 
     var post1 = 'TestsetId=' + TestsetId + '&TestsetName=' + TestsetName;
     console.log(TestsetName);
@@ -186,6 +195,26 @@ function CheckLoginStatus() {
                     $('#UserLevel').html('Your Level : ' + data[i].UserLevel + '<br />');
 
                 }
+            }
+        }
+    });
+}
+//20240730 สร้าง Dropdown สำหรับเลือกระดับชั้นที่ต้องการให้แสดงชุดข้อสอบ
+function GetLevel() {
+  
+    $.ajax({
+        type: 'POST',
+        url: '/weTest/GetLevel',
+        success: function (data) {
+            var selectHTML = "";
+            if (data[0].result == 'success') {
+                selectHTML = '<select id="ddlLevel">';
+
+                for (var i = 0; i < data.length; i++) {
+                    selectHTML += "<option value='" + data[i].LevelId + "'>" + data[i].LevelName + "</option>";
+                }
+                selectHTML += "</select>";
+                $('#SelectLevel').html(selectHTML);
             }
         }
     });
