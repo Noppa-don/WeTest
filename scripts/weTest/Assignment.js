a376a3ef74b86f3ddf4e69ff155ef1054acd15f5
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-08 05:23:50 +0700

    Create Assignment.js

---
+++ scripts/weTest/Assignment.js
@@ -0,0 +1,14 @@
+﻿var ExamAmount, skill
+// ========================= Page Load ======================== //
+$(function () { $('div[data-role=page]').page({ theme: 'c', }); });
+
+// ============================================================ //
+// ======================= Object Event ======================= //
+$(document)
+// ======================= Activity ======================= //
+.on('click', '.btnBack', function (e, data) {
+        window.location = '/Wetest/User';
+})
+
+
+
