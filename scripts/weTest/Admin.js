67aeada8bbe338f781b6ca269e74049184728c8a
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-16 03:11:04 +0700

    Create Admin.js

---
+++ scripts/weTest/Admin.js
@@ -0,0 +1,44 @@
+﻿// ========================= Page Load ====================================================================== //
+$(function () {
+    $('div[data-role=page]').page({ theme: 'c', });
+    $('#userName').focus();
+});
+
+// ========================================================================================================== //
+
+// ============================================== Object Event ============================================== //
+$(document)
+// ======================== Login ============================== //
+    .on('focus', '#userName, #userPass', function (e, data) { $(this).removeClass("InvalidData") })
+    .on('keypress blur', '#userName, #userPass', function (e, data) {
+        var xkey = keyA(e);
+        if (xkey == 13) { keyEnterNextItem(e); }
+    })
+    .on('keypress blur', '#ConfirmPassword', function (e, data) {
+        var xkey = keyA(e);
+        if (xkey == 13) { checkLogin(); }
+    })
+    .on('click', '#btnLogin', function (e, data) {
+        if (checkInvalidLoginData() == 'true') {
+            CheckUserLogin();
+        }
+    })
+
+function CheckUserLogin() {
+    $.ajax({
+        type: 'POST',
+        url: '/weTest/CheckUserLogin',
+        success: function (data) {
+            for (var i = 0; i < data.length; i++) {
+
+                if (data[i].dataType == 'success') {
+                }
+
+            }
+        }
+    });
+}
+
+
+
+
