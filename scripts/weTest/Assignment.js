f3693e9e50b8481e042889aae18c1d5763f9b715
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-15 08:34:43 +0700

    Update Assignment.js

--- scripts/weTest/Assignment.js
+++ scripts/weTest/Assignment.js
@@ -9,6 +9,13 @@ $(document)
 .on('click', '.btnBack', function (e, data) {
     window.location = '/Wetest/User';
 })
+.on('click', '.redAssignment,.orangeAssignment,.greenAssignment', function (e, data) {
+    var TestsetId = $(this).attr('refId');
+    var TestsetName = $(this).attr('refName');
+    GotoAssignment(TestsetId, TestsetName);
+})
+
+GotoAssignment
 
 //20240813 -- Get Assignment
 function GetAssignment() {
@@ -18,13 +25,33 @@ function GetAssignment() {
         success: function (data) {
             for (var i = 0; i < data.length; i++) {
                 if (data[i].Result == 'success') {
-                    console.log(data[i].OverDue);
                     $('#OverdueItem').html(data[i].OverDue);
                     $('#TodayItem').html(data[i].Today);
                     $('#ThisWeekItem').html(data[i].ThisWeek);
                     $('#NextWeekItem').html(data[i].NextWeek);
                 }
 
+            }
+        }
+    });
+}
+//20240814 -- Go to Assignment
+function GotoAssignment(TestsetId, TestsetName) {
+    console.log(TestsetId);
+    console.log(TestsetName);
+    var post1 = 'TestsetId=' + TestsetId + '&TestsetName=' + TestsetName;
+    $.ajax({
+        type: 'POST',
+        url: '/weTest/CreateAssignment',
+        data: post1,
+        success: function (data) {
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].dataType == 'success') {
+                    var x = setInterval(function () {
+                        clearInterval(x);
+                        window.location = '/Wetest/Activity';
+                    }, 1000);
+                }
             }
         }
     });
