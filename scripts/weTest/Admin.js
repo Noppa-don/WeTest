eb4f15187d403f38d5fd4c191644248d5dea2d3c
Noppa-don <noppadon.sununkingphet@gmail.com>
2024-08-22 07:31:08 +0700

    Update Admin.js

--- scripts/weTest/Admin.js
+++ scripts/weTest/Admin.js
@@ -28,6 +28,44 @@ $(document)
         $('.PaymentList').removeClass('ui-hide');
         GetJobDetail();
     })
+    .on('click', '.jobDetailItem', function (e, data) {
+        var RHId = $(this).attr('RHId');
+        ShowJobDetail(RHId)
+        $('.PaymentList').addClass('ui-hide');
+        $('.PaymentDetail,.footerslip').removeClass('ui-hide');
+
+    })
+    //20240820 -- cancel confirm slip
+    .on('click', '.btnCancelConfirmSlip', function (e, data) {
+        $('.PaymentList').removeClass('ui-hide');
+        $('.PaymentDetail,.footerslip').addClass('ui-hide');
+
+    })
+    //20240820 -- confirm slip
+    .on('click', '.btnConfirmSlip', function (e, data) {
+        $('#dialogConfirm').attr('action', 'focus');
+        popupOpen($('#dialogConfirm'), 99999);
+    })
+    .on('click', '#dialogConfirm .btnconfirm', function (e, data) {
+        var RHId = $(this).attr('RHId');
+        SaveConfirmSlip(RHId,2);
+        popupClose($(this).closest('.my-popup'));
+    })
+
+    //20240820 -- reject slip
+    .on('click', '.btnReject', function (e, data) {
+        $('#dialogReject').attr('action', 'focus');
+        popupOpen($('#dialogReject'), 99999);
+     })
+    .on('click', '#dialogReject .btnRejectConfirm', function (e, data) {
+        var RHId = $(this).attr('RHId');
+        SaveConfirmSlip(RHId, 3);
+        popupClose($(this).closest('.my-popup'));
+    })
+
+    .on('click', '#dialogConfirm .btnNo,#dialogReject .btnNo', function (e, data) {
+        popupClose($(this).closest('.my-popup'));
+    })
 
 
 function CheckUserLogin() {
@@ -44,7 +82,6 @@ function CheckUserLogin() {
         }
     });
 }
-
 function GetJobDetail() {
     $.ajax({
         type: 'POST',
@@ -55,6 +92,41 @@ function GetJobDetail() {
                 if (data[i].dataType == 'success') {
                     $('.JobDetail').html(data[i].errorMsg);
                 }
+
+            }
+        }
+    });
+}
+//20240820 -- ดึงข้อมูลรายละเอียดสลิป
+function ShowJobDetail(RHId) {
+    var data = 'RHId=' + RHId
+    $.ajax({
+        type: 'POST',
+        url: '/weTest/GetSlip',
+        data: data,
+        success: function (data) {
+            for (var i = 0; i < data.length; i++) {
+
+                if (data[i].Result == 'success') {
+                    $('.slipDetail').html(data[i].ResultTxt);
+                    $('.slipPhoto').css('background-image', 'url(' + data[i].slipURL + ')');
+                    $('#dialogConfirm .btnconfirm').attr('RHId', RHId);
+                    $('#dialogReject .btnRejectConfirm').attr('RHId', RHId);
+                }
+            }
+        }
+    });
+}
+//20240820 -- บันทึกสถานะ
+function SaveConfirmSlip(RHId,RegisterStatus) {
+    var data = 'RHId=' + RHId + '&RegisterStatus=' + RegisterStatus
+    $.ajax({
+        type: 'POST',
+        url: '/weTest/ConfirmSlip',
+        data: data,
+        success: function (data) {
+            for (var i = 0; i < data.length; i++) {
+
 
             }
         }
